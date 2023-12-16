98ceeed0bea80e3a1cc1547cbca4418e5d60ecf2
Nabis <r.cernil332@gmail.com>
2023-12-16 18:22:39 +0100

    Add project files.

---
+++ Dorian´s Calculator/Class1.cs
@@ -0,0 +1,57 @@
+﻿
+using System.Runtime.CompilerServices;
+
+namespace Dorian_s_Calculator
+{
+    public class Calculator
+    {
+
+        private string _lastOperation;
+        private double _lastResult;
+        private string _name;
+
+        public Calculator(string name)
+        {
+            _name = name;
+        }
+        public double Sum(double number1, double number2)
+        {
+            _lastOperation = "Sum";
+            _lastResult = number1 + number2;
+            return _lastResult;
+        }
+
+        public double Subtract(double number1, double number2)
+        {
+            _lastOperation = "Subtract";
+            _lastResult = number1 - number2;
+            return _lastResult;
+        }
+
+        public double Multiply(double number1, double number2)
+        {
+            _lastOperation = "Multiply";
+            _lastResult = number1 * number2;
+            return _lastResult;
+        }
+
+        public double Divide(double dividend, double divisor)
+        {
+            _lastOperation = "Divide";
+            _lastResult = dividend / divisor;
+            return _lastResult;
+        }
+
+        public double GetLastResult()
+        {
+            return _lastResult;
+        }
+        public string GetLastOperation()
+        {
+            return _lastOperation;
+        }
+
+    }
+        
+    
+}
---
+++ Dorian´s Calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Dorian_s_Calculator
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator myCalculator = new Calculator("My Calculator");
+
+            double sumResult = myCalculator.Sum(6, 9);
+            Console.WriteLine($"Addition result: {sumResult}");
+
+            double subtractResult = myCalculator.Subtract(10, 6);
+            Console.WriteLine($"Substraction result: {subtractResult}");
+
+            double multiplyResult = myCalculator.Multiply(7, 7);
+            Console.WriteLine($"Multiply result: {multiplyResult}");
+
+            double divideResult = myCalculator.Divide(8, 2);
+            Console.WriteLine($"Division result: {divideResult}");
+
+            string lastOperation = myCalculator.GetLastOperation();
+            double lastResult = myCalculator.GetLastResult();
+            Console.WriteLine($"Last operation performed: {lastOperation}");
+            Console.WriteLine($"Last result: {lastResult}");
+        }
+    }
+}
